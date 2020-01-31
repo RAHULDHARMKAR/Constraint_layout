026ec5ff4cf62885d3f8a2d534de812709d5a719
Rahul <rahuldharmkar@gmail.com>
2020-01-30 21:15:46 -0800

    Constraint Layout

---
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button(View view) {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
+    }
+}
